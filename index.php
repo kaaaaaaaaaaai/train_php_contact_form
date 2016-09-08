2090eb56bdce69bc1e92ceffcae3604b63d19224
kai ogita <kai-ogita@rich.co.jp>
2016-09-08 22:55:54 +0900

    view render

--- index.php
+++ index.php
@@ -3,5 +3,9 @@
 require_once __DIR__."/vendor/autoload.php";
 require_once __DIR__."/Util.php";
 
-$r = new App\Http\route();
-$r->dispatch();
+$r = new App\Http\route(new \App\Http\Request());
+try{
+    $r->dispatch();
+}catch (HttpException $e){
+    
+}
