990eeb1fad2a36d7615b3b1290dad3c24aa452be
kai ogita <kai-ogita@rich.co.jp>
2016-09-10 23:58:31 +0900

    add getReferermethod

--- app/Http/Request.php
+++ app/Http/Request.php
@@ -4,41 +4,30 @@ namespace App\Http;
 
 class Request{
 
-    protected $query;
-    protected $request;
-    protected $httpMethod;
-
-    public function __construct()
-    {
-        foreach ($_GET as $key => $value) {
-            $this->query[$key]  =   $value;
-        }
-        foreach ($_POST as $key => $value) {
-            $this->request[$key]  = $value;
-        }
-        $this->httpMethod = $_SERVER["REQUEST_METHOD"];
-    }
-
     public function getQuery($key){
-        return $this->query[$key];
+        return $this->hasQuery($key)?$_GET[$key]:null;
     }
 
     public function getPost($key){
-        return $this->request[$key];
+        return $this->hasPost($key)?$_POST[$key]:null;
     }
 
     public function hasQuery($key){
-        return isset($this->query[$key])?true:false;
+        return isset($_GET[$key])?true:false;
     }
     public function hasPost($key){
-        return isset($this->request[$key])?true:false;
+        return isset($_POST[$key])?true:false;
     }
 
     public function getPostAll(){
-        return $this->request;
+        return $_POST;
     }
 
     public function getRequestMethod(){
-        return $this->httpMethod;
+        return $_SERVER["REQUEST_METHOD"];
+    }
+
+    public function getReferer(){
+        return $_SERVER['HTTP_REFERER'];
     }
 }
