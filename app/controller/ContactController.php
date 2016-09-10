869bb22e27740a7243b2f862fab5b82512b50f04
kai-ogita <hotsplusph@gmail.com>
2016-09-10 12:30:36 +0900

    fix

---
+++ app/controller/ContactController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use App\Http\Request;
+
+class ContactController{
+    public function get_index(){
+        //templateを読み込むため
+        view("view.input");
+    }
+
+    public function post_confirm(){
+        $request    = new Request();
+        view('view.confirm',$request->getPostAll());
+    }
+}
