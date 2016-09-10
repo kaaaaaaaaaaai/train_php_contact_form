79cb3fd4efc2f8132c5daa3b312ca539b8445acb
kai ogita <kai-ogita@rich.co.jp>
2016-09-10 23:57:14 +0900

    edit actionName, fix validate

--- app/controller/ContactController.php
+++ app/controller/ContactController.php
@@ -3,14 +3,32 @@
 namespace App\Controller;
 
 use App\Http\Request;
+use App\Validation\Validate;
 
 class contactController{
-    public function get_index(){
+    public function get_input(){
         //templateを読み込むため
-        view("view.input");
+        view("view/input");
     }
 
-    public function post_confirm($request){
-        view('view.confirm',$request->getPostAll());
+    public function post_confirm(Request $request){
+        $validate = new Validate();
+        $validate->setData($request->getPostAll());
+        $rules = [
+            'name'      =>  'required|max:255',
+            'gender'    =>  'required',
+            'comment'   =>  'max:255',
+            'email'     =>  'required|email',
+        ];
+        //set
+        $validate->setRules($rules);
+
+        if(!$validate->run()){
+            $error["error"] = $validate->getMessage();
+            view("view/input", $error);
+        }else{
+            view('view/confirm', $request->getPostAll());
+        }
+
     }
 }
